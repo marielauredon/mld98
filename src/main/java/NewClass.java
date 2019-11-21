695bf015ff3590c9df55c9cd6e34a280915bc99f
cbenait <cbenait@cst-tp-b10306>
2019-11-21 09:17:27 +0100

    Ajout setNom et setPrenom

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -12,15 +12,22 @@ public class NewClass {
     private String nom;
     private String prenom;
 
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
     
-
-
-public String getNom() {
-    return nom;
-}
-
-public String getPrenom(){
-    return prenom;
-}
 
 }
